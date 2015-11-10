42d4d4aea174ee630eca54edb1b46d1dfe84ec8b
Cole Willsea <coleww@gmail.com>
2015-11-09 22:28:07 -0800

    its been a good day, good procjamming, yeah, nice

--- sequencer/index.js
+++ sequencer/index.js
@@ -1,5 +1,5 @@
 var createInstruments = require('./instruments');
-
+var pick = require('pick-random')
 function getTick(bpm){
   return ((60 * 1000) / bpm) / 4;
 }
@@ -16,13 +16,21 @@ var Sequencer = function(data){
   console.log(this.lines)
   this.section = data.section || "intro"
   this.sections = data.sections || ["verse", "verse", "verse", "verse", "chorus", "chorus"]
+
+  this.nextSections = {
+    "intro": ["intro", "verse"],
+    "verse": ["verse", "verse", "chorus"],
+    "chorus": ["chorus", "chorus", "verse", "bridge", "outro"],
+    "bridge": ["bridge", "chorus", "outro"],
+    "outro": ["outro", false]
+  }
+
+
   this.position = 0;
   this.steps = data.steps;
-  this.savedLik = 'ooooweeeeooooo'
 };
 
 Sequencer.prototype.run = function(){
-  document.getElementById("karaoke").textContent = this.lines.shift()
   var that = this;
   var tick = getTick(that.bpm);
   this.interval = window.setInterval(function(){
@@ -40,7 +48,11 @@ Sequencer.prototype.run = function(){
         instrument.next(that.section);
       });
       that.position = 0;
-      that.section = that.sections.shift()
+
+
+      // RIGHT HERE DO THAT MAGIC!
+      // MAKE THE SECTIONS THE MAGIC!
+      that.section = pick(that.nextSections[that.section])[0]
 
       if(!that.section) {
         that.stop()
---
+++ www/index.js
@@ -0,0 +1,1 @@
+var seq = require('../sequencer')
