0490a7274bb95a3a536650c0e4280c6be840b76b
Cole Willsea <coleww@gmail.com>
2015-11-14 10:36:06 -0800

    ah yes

--- sequencer/synther.js
+++ sequencer/synther.js
@@ -12,7 +12,6 @@ module.exports = function (ac) {
     audioNodes: samples,
     start: function (when, arg, shift) {
       arg.forEach(function (arg) {
-
         samples[arg].pitchBend.setValueAtTime(shift, when)
         samples[arg].start(when)
       })
